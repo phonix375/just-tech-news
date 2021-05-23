a24f3cdd57d56a11b545a69eb897b2016178810e
Alexy Kotliar <kotliar.alex@gmail.com>
2021-05-22 20:19:53 -0400

    added hash to the passwords

--- models/user.js
+++ models/user.js
@@ -1,8 +1,10 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 //create our User model 
 class User extends Model{}
+
 
 //define table columns and configuration 
 User.init(
@@ -40,9 +42,21 @@ User.init(
                 //this means the password must be at least four character long
                 len:[4]
             }
-        }
+        },
     },
     {
+        hooks: {
+            //set up before create lifecycle "hool" functionality
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password,10);
+                return newUserData;
+            },
+            //set up beforeUpdate lifecycle "hook" functionality
+            async beforeUpdate(updateUserData){
+                updateUserData.password = await bcrypt.hash(updateUserData.password, 10);
+                return updateUserData;
+            }
+        },
         //table configuration option go here
 
         //pass in our imported sequelize connection (the direct connection to our database)
